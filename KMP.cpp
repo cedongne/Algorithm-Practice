ea6517429bac2befea681e5c2a821a333ef64c9c
cedongne <57585303+cedongne@users.noreply.github.com>
2021-12-29 00:58:48 +0900

    KMP Algorithm

---
+++ KMP.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> MakeTable(string pattern) {
+	int patternSize = pattern.size();
+	vector<int> table(patternSize, 0);
+
+	int start = 0;
+	for (int end = 1; end < patternSize; end++) {
+		while (start > 0 && pattern[start] != pattern[end]) {
+			start = table[start - 1];
+		}
+		if (pattern[start] == pattern[end]) {
+			table[end] = ++start;
+		}
+	}
+
+	return table;
+}
+
+vector<int> KMP(string parent, string pattern) {
+	int parentSize = parent.size();
+	int patternSize = pattern.size();
+
+	vector<int> table = MakeTable(pattern);
+
+	int patternCount = 0;
+
+	vector<int> found_index;
+	for (int parentCount = 0; parentCount < parentSize; parentCount++) {
+		while (patternCount > 0 && parent[parentCount] != pattern[patternCount]) {
+			patternCount = table[patternCount - 1];
+		}
+		if (parent[parentCount] == pattern[patternCount]) {
+			if (patternCount == patternSize - 1) {
+				found_index.push_back(parentCount - patternSize + 2);
+				patternCount = table[patternCount];
+			}
+			else {
+				patternCount++;
+			}
+		}
+	}
+	return found_index;
+}
+
+void PrintPatternIndex(vector<int> found_index) {
+	for (int count = 0; count < found_index.size(); count++) {
+		printf("%d 위치에 문자열이 존재합니다.\n", found_index[count]);
+	}
+}
+
+int main() {
+	string parent = "ababacabacaabacaaba";
+	string pattern = "abacaaba";
+	vector<int> found_indexes = KMP(parent, pattern);
+
+	PrintPatternIndex(found_indexes);
+	found_indexes.clear();
+
+	parent = "AAAAAAAAAB";
+	pattern = "AAB";
+	found_indexes = KMP(parent, pattern);
+
+	PrintPatternIndex(found_indexes);
+	found_indexes.clear();
+}
