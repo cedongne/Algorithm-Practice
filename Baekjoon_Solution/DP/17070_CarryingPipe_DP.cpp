77fd2171b53cbfe35bbc3c63d44d3f2b88656117
cedongne <cedongne@gmail.com>
2022-06-08 13:36:28 +0900

    DP

--- Baekjoon_Solution/DP/17070_CarryingPipe_DP.cpp
+++ Baekjoon_Solution/DP/17070_CarryingPipe_DP.cpp
@@ -1,50 +1,37 @@
 #include <iostream>
 #include <queue>
-#include <vector>
+#define R	0
+#define I	1
+#define D	2
 
-#define horizontal	1
-#define diagonal	2
-#define vertical	3
-
-struct move {
-	int row;
-	int col;
-	int direction;
-};
-
-std::queue<move> q;
-std::pair<int, int> direction[3] = { {1, 0}, {1, 1}, {0, 1} };
+std::pair<int, int> direction[3] = { {0, 1}, {1, 1}, {1, 0} };
+int map[18][18] = { 0, };
 
 int n;
 
-int map[18][18];
-int visit[18][18] = { 0, };
+int wayCnt = 0;
 
-void bfs() {
-	while (!q.empty()) {
-		move cur = q.front();
-		q.pop();
-		if (cur.direction != vertical) {
-			int nextRow = cur.row;
-			int nextCol = cur.col + 1;
-			if (map[nextRow][nextCol] == 0) {
-				visit[nextRow][nextCol]++;
-				q.push({ nextRow, nextCol, horizontal });
-			}
+void dfs(int row, int col, int dir) {
+	if (row == n && col == n) {
+		wayCnt++;
+	}
+	for (int nextDir = 0; nextDir < 3; nextDir++) {
+		if ((nextDir == R && dir == D) || (nextDir == D && dir == R)) {
+			continue;
 		}
-		int nextRow = cur.row + 1;
-		int nextCol = cur.col + 1;
-		if (map[nextRow][nextCol] == 0 && map[cur.row][nextCol] == 0 && map[nextRow][cur.col] == 0) {
-			visit[nextRow][nextCol]++;
-			q.push({ nextRow, nextCol, diagonal });
+		int nextRow = row + direction[nextDir].first;
+		int nextCol = col + direction[nextDir].second;
+		if (nextRow == n + 1 || nextCol == n + 1 || map[nextRow][nextCol] == 1) {
+			continue;
 		}
-		if (cur.direction != horizontal) {
-			int nextRow = cur.row + 1;
-			int nextCol = cur.col;
-			if (map[nextRow][nextCol] == 0) {
-				visit[nextRow][nextCol]++;
-				q.push({ nextRow, nextCol, vertical });
-			}
+		if (nextDir == I && map[row][nextCol] == 0 && map[nextRow][col] == 0) {
+			dfs(nextRow, nextCol, I);
+		}
+		if (nextDir == R && map[nextRow][nextCol] == 0) {
+			dfs(nextRow, nextCol, R);
+		}
+		if (nextDir == D && map[nextRow][nextCol] == 0) {
+			dfs(nextRow, nextCol, D);
 		}
 	}
 }
@@ -56,21 +43,12 @@ int main() {
 
 	std::cin >> n;
 
-	for (int row = 0; row <= n + 1; row++) {
-		for (int col = 0; col <= n + 1; col++) {
-			map[row][col] = -1;
-		}
-	}
-
 	for (int row = 1; row <= n; row++) {
 		for (int col = 1; col <= n; col++) {
 			std::cin >> map[row][col];
 		}
 	}
+	dfs(1, 2, R);
 
-	q.push({ 1, 2, horizontal });
-	visit[1][2] = 1;
-	bfs();
-
-	std::cout << visit[n][n];
+	std::cout << wayCnt;
 }
